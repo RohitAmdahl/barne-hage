98eafb5bc55859b0ac50c14bf4f3212526d9cb4e
rohitAmdahl <rohit_designer@outlook.com>
2024-03-17 01:48:49 +0100

    footer links made

---
+++ utils/footerNav.ts
@@ -0,0 +1,32 @@
+interface FooterNav {
+  name: string;
+  path: string;
+}
+
+export const footerLinks: FooterNav[] = [
+  { name: "Home", path: "/" },
+  {
+    name: "Indoor",
+    path: "/indoor",
+  },
+  {
+    name: "Opening Hours",
+    path: "/opening-hours",
+  },
+  {
+    name: "Parents Advice",
+    path: "/parents-advice",
+  },
+  {
+    name: "Recordings",
+    path: "/recording",
+  },
+  {
+    name: "Team partners",
+    path: "/team-partners",
+  },
+  {
+    name: "Who are vi",
+    path: "/who-are-vi",
+  },
+];
