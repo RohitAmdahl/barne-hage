4f85decf5d501c4117b40d1e020ec6abc521f39a
rohitAmdahl <rohit_designer@outlook.com>
2024-03-17 01:33:57 +0100

    navigation file

---
+++ utils/navlinks.ts
@@ -0,0 +1,24 @@
+interface Nav {
+  name: string;
+  path: string;
+}
+
+export const navLinks: Nav[] = [
+  { name: "Home", path: "/" },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Educational",
+    path: "/educational",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "Day Routine",
+    path: "/dayrutine",
+  },
+];
