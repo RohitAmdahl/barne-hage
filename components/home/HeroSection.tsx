f24b453dac83cc19970e13bc22ffc7584dadc9af
rohitAmdahl <rohit_designer@outlook.com>
2024-03-17 01:36:01 +0100

    heroSection file update , still working on

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata = {
+  title: "Home page!",
+  description: "home page about barnehage",
+};
+
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className=" text-2xl font-mono font-bold container max-w-5xl mx-auto text-center ">
+      <h1>HeroSection</h1>
+    </div>
+  );
 };
 
 export default HeroSection;
