825fb1dd5a9b408e2624d5b978fb9830f70f2682
rohitAmdahl <rohit_designer@outlook.com>
2024-03-17 01:49:56 +0100

    -navbar testing

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,13 +1,7 @@
 const Navbar = () => {
   return (
     <div>
-      {/* <ul>
-        <li>navigation</li>
-        <li>navigation</li>
-        <li>navigation</li>
-        <li>navigation</li>
-        <li>navigation</li>
-      </ul> */}
+      <ul></ul>
     </div>
   );
 };
