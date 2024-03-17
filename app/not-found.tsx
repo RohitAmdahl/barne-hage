d0986b32921b658e5ed1d36a9f1e3b01ec93e605
rohitAmdahl <rohit_designer@outlook.com>
2024-03-17 01:35:08 +0100

    page not file added

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="mx-auto container bg-red-400 max-w-4xl ">
+      <h1>not-found 404 page </h1>
+      <h1>not-found 404 page </h1>
+      <h1>not-found 404 page </h1>
+    </div>
+  );
+};
+
+export default NotFound;
