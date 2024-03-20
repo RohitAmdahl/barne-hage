a290d9af127fd0a8c545ff554cfcd16e3109844a
rohitAmdahl <rohit_designer@outlook.com>
2024-03-20 01:03:59 +0100

    footer component file made

---
+++ components/footer/footer.tsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+  return (
+    <div className="m-4 ">
+      <h1 className=" bg-green-200 text-2xl font-mono font-bold  text-center ">
+        Footer
+      </h1>
+    </div>
+  );
+};
+
+export default FooterComponent;
