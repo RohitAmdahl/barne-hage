1865bc8f990a0a0f2ab6c05a9980c12c898bd461
rohitAmdahl <rohit_designer@outlook.com>
2024-03-23 01:51:03 +0100

    padding style

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div className="container mx-auto max-w-5xl p-4">
+    <div className="container mx-auto max-w-5xl p-2">
       <header className="flex flex-wrap justify-between items-baseline">
         <Logo />
         <Navbar />
