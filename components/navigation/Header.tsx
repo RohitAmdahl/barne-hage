cc607b42f834df8250a0e78d286ef4b58f618a91
rohitAmdahl <rohit_designer@outlook.com>
2024-03-23 18:55:27 +0100

    header color changed

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -3,11 +3,13 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div className="container mx-auto max-w-5xl p-2">
-      <header className="flex flex-wrap justify-between items-baseline">
-        <Logo />
-        <Navbar />
-      </header>
+    <div className="bg-pink-200 ">
+      <div className="container mx-auto max-w-5xlp-2">
+        <header className="flex flex-wrap justify-between items-baseline">
+          <Logo />
+          <Navbar />
+        </header>
+      </div>
     </div>
   );
 };
