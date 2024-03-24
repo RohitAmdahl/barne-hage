c5a600855b620cb95f464e3b11e6d2681ce18dce
rohitAmdahl <rohit_designer@outlook.com>
2024-03-24 22:58:28 +0100

    update

---
+++ app/openingstime/page.tsx
@@ -0,0 +1,5 @@
+const OpensTime = () => {
+  return <div>page</div>;
+};
+
+export default OpensTime;
---
+++ app/outdoor/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OutDoorArea = () => {
+  return <div>OutDoorArea</div>;
+};
+
+export default OutDoorArea;
