07301df374386ad6b22a6974f81ab80652ec1ddd
rohitAmdahl <rohit_designer@outlook.com>
2024-03-24 20:49:08 +0100

    added styles

--- components/home/TabsLinks.tsx
+++ components/home/TabsLinks.tsx
@@ -1,41 +1,47 @@
 import Link from "next/link";
+import WhyChooseUs from "./WhyChooseUs";
 
 const TabsLinks = () => {
   return (
-    <div className=" p-2  grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4 ">
-      <div className="bg-green-300 text-center p-5 rounded-lg hover:ring-2">
-        <Link
-          href={"/dayrutine"}
-          className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
-        >
-          Day Routine at BarneHage
-        </Link>
-      </div>
-      <div className=" bg-fuchsia-300 text-center p-5 rounded-lg hover:ring-2 ">
-        <Link
-          href={"/indoor"}
-          className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
-        >
-          Indoor at BarneHage
-        </Link>
-      </div>
-      <div className=" bg-pink-400 text-center p-5 rounded-lg hover:ring-2">
-        <Link
-          href={"/parentsadvice"}
-          className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
-        >
-          Parents Advice
-        </Link>
-      </div>
-      <div className=" bg-orange-300 text-center p-5 rounded-lg hover:ring-2 ">
-        <Link
-          href={"/timing"}
-          className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
-        >
-          BarneHage Timing
-        </Link>
-      </div>
-    </div>
+    <>
+      <section>
+        <div className=" p-2  grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4 ">
+          <div className="bg-green-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-green-200">
+            <Link
+              href={"/dayrutine"}
+              className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
+            >
+              Day Routine at BarneHage
+            </Link>
+          </div>
+          <div className=" bg-fuchsia-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-fuchsia-200 ">
+            <Link
+              href={"/indoor"}
+              className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
+            >
+              Indoor at BarneHage
+            </Link>
+          </div>
+          <div className=" bg-pink-400 text-center p-5 rounded-lg hover:ring-2 hover:bg-pink-300">
+            <Link
+              href={"/parentsadvice"}
+              className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
+            >
+              Parents Advice
+            </Link>
+          </div>
+          <div className=" bg-orange-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-orange-200 ">
+            <Link
+              href={"/timing"}
+              className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
+            >
+              BarneHage Timing
+            </Link>
+          </div>
+        </div>
+        <WhyChooseUs />
+      </section>
+    </>
   );
 };
 
