00d538f5b1ad5a20dc123b6781798822ad916082
rohitAmdahl <rohit_designer@outlook.com>
2024-03-24 20:49:57 +0100

    added classes

--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 const Logo = () => {
   return (
-    <div>
-      <Image src="/logo.png" alt="logo barnehage" width={180} height={100} />
+    <div className=" flex justify-center items-center">
+      <Image
+        className="max-w-2xl container mx-auto"
+        src="/logo.png"
+        alt="logo barnehage"
+        width={180}
+        height={100}
+      />
     </div>
   );
 };
