59a334eb4ae30326b2048e7843c702cdffec159f
rohitAmdahl <rohit_designer@outlook.com>
2024-03-25 20:03:59 +0100

    ts for cards

---
+++ utils/cards.ts
@@ -0,0 +1,44 @@
+interface TeamsCards {
+  name: string;
+  role?: string;
+  image: string;
+  education: string;
+}
+
+export const teamsCards: TeamsCards[] = [
+  {
+    name: "Merve Kavak",
+    image: "/lady.jpg",
+    role: "Kids Teacher & Coordinator",
+    education: "Barnehage Teacher",
+  },
+  {
+    name: "Nina Karlson",
+    image: "/ladyred.jpg",
+    role: "Kids Teacher & Coordinator",
+    education: "Assistent Teacher",
+  },
+  {
+    name: "Marcus Olsen",
+    image: "/man.jpg",
+    education: "Barnehage Teacher",
+  },
+  {
+    name: "Jessica smith Olsen",
+    image: "/owner.jpg",
+    role: "Co-Founder/ Owner ",
+    education: "Barnehage Teacher",
+  },
+  {
+    name: "Jessica smith Olsen",
+    image: "/assistent.jpg",
+    role: "Assistant ",
+    education: "Barnehage Teacher",
+  },
+  {
+    name: "Anne Cathrine",
+    image: "/assistent1.jpg",
+    role: "Assistant",
+    education: "Assistent Teacher",
+  },
+];
