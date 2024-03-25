a7bdb01a83b4f6731a19cedd09c890579f2a410a
rohitAmdahl <rohit_designer@outlook.com>
2024-03-25 21:54:40 +0100

    interface about page

---
+++ utils/aboutPage.ts
@@ -0,0 +1,22 @@
+interface AboutPage {
+  title: string;
+  description: string;
+}
+
+export const aboutText: AboutPage[] = [
+  {
+    title: "Magic of Our Barnehage.",
+    description:
+      "Welcome to our kindergarten, where every child's journey begins with wonder and excitement, our colorful classrooms and engaging outdoor spaces provide a safe and nurturing environment for exploration and discovery. With a dedicated team of educators, we encourage curiosity, creativity, and friendship. Together with families, we create a vibrant community where children thrive and grow. Join us on this adventure of learning and laughter at our kindergarten.",
+  },
+  {
+    title: "Why Choose Us.",
+    description:
+      "First day at daycare. Butterflies in the tummy. Excited and a tad apprehensive. Will I find a playmate? Will it be enjoyable? At FUS daycare, your child will feel secure, thrive, play, and grow. They'll explore and learn about themselves and others",
+  },
+  {
+    title: "Playful Learning",
+    description:
+      "At our center, we embrace the power of play to fuel children's curiosity and learning. Through engaging activities and interactive experiences, we cultivate a stimulating environment where every moment is an opportunity for discovery. Our innovative approach integrates play seamlessly into our curriculum, fostering creativity, problem-solving skills, and social development. From sensory exploration to imaginative play, each activity is designed to ignite children's passion for learning. Our dedicated educators provide guidance and support, encouraging children to explore and learn at their own pace. Join us as we embark on a journey of playful learning, where every child's potential shines bright",
+  },
+];
