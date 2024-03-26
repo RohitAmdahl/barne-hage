6503fda2242292c666d23b7216a11e3802c39cb0
rohitAmdahl <rohit_designer@outlook.com>
2024-03-26 01:41:06 +0100

    made slider array og object

---
+++ utils/sliderImg.ts
@@ -0,0 +1,26 @@
+interface SliderImg {
+  id: number;
+  image: string;
+  alt: string;
+  description: string;
+}
+export const sliderImg: SliderImg[] = [
+  {
+    id: 1,
+    image: "/slider1.png",
+    alt: "slider image",
+    description: "slider image",
+  },
+  {
+    id: 2,
+    image: "/slider2.png",
+    alt: "slider image",
+    description: "slider image",
+  },
+  {
+    id: 3,
+    image: "/slider3.png",
+    alt: "slider image",
+    description: "slider image",
+  },
+];
