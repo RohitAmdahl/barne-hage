d2d83fdea9f47b17d305cd706792636eed403767
rohitAmdahl <rohit_designer@outlook.com>
2024-03-26 17:55:02 +0100

    aded header styles

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="w-full px-4 fixed  backdrop-blur-xl bg-white/30   ">
-      <div className=" p-2 max-w-5xl mx-auto container ">
-        <div className=" flex flex-wrap justify-center items-center lg:justify-between md:justify-between ">
-          <Navbar />
+    <header>
+      <div className="w-full px-4 fixed  backdrop-blur-xl bg-white/30">
+        <div className=" p-2 max-w-5xl mx-auto container">
+          <div className=" flex flex-wrap justify-center items-center lg:justify-between md:justify-between ">
+            <Navbar />
+          </div>
         </div>
       </div>
     </header>
--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import logImg from "@/public/logo.png";
 const Logo = () => {
   return (
-    <Image
-      className="w-full container mx-auto max-w-md md:max-w-lg lg:max-w-xl"
-      src={logImg}
-      alt="logo barnehage"
-    />
+    <div>
+      <Image
+        className="max-w-md md:max-w-xl lg:max-w-xl"
+        src={logImg}
+        alt="logo barnehage"
+      />
+    </div>
   );
 };
 
