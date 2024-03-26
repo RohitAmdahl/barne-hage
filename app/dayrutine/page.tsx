02d6fbd18566dcc6a826335311422deaf15341ba
rohitAmdahl <rohit_designer@outlook.com>
2024-03-26 23:18:07 +0100

    coponent added

--- app/dayrutine/page.tsx
+++ app/dayrutine/page.tsx
@@ -1,9 +1,11 @@
+import DaysInBarnehage from "@/components/dayRoutineInbarnehage/DaysInBarnehage";
+
 const NormalDaysRoutine = () => {
   return (
     <div>
-      <h1 className=" text-2xl font-mono font-bold container max-w-5xl mx-auto text-center ">
-        NormalDaysRoutine page
-      </h1>
+      <div>
+        <DaysInBarnehage />
+      </div>
     </div>
   );
 };
