64d8202111064e98735b462a571bc947aace09f5
rohitAmdahl <rohit_designer@outlook.com>
2024-03-27 22:39:17 +0100

    edit padding

--- components/home/TabsLinks.tsx
+++ components/home/TabsLinks.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const TabsLinks = () => {
   return (
-    <div>
+    <div className="pt-8">
       <div className=" p-2  grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4  ">
-        <div className="bg-green-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-green-200 ">
+        <div className="bg-green-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-green-200">
           <Link
             href={"/dayrutine"}
             className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
