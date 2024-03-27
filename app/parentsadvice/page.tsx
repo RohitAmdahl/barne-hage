2e8cd378c1c5b4cc11f295755d39a9485dd2a140
rohitAmdahl <rohit_designer@outlook.com>
2024-03-27 22:15:51 +0100

    advice page components added

--- app/parentsadvice/page.tsx
+++ app/parentsadvice/page.tsx
@@ -1,5 +1,17 @@
+import Advice from "@/components/advice/Advice";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Parents Advice page! Barnehage",
+  description: "parents advice page barnehage",
+};
+
 const ParentsAdvice = () => {
-  return <div>ParentsAdvice</div>;
+  return (
+    <div className="pt-8 container mx-auto max-w-4xl ">
+      <Advice />
+    </div>
+  );
 };
 
 export default ParentsAdvice;
---
+++ components/advice/Advice.tsx
@@ -0,0 +1,5 @@
+const Advice = () => {
+  return <div className="pt-32 md:pt-24 lg:pt-24 ">Advice</div>;
+};
+
+export default Advice;
