c8ef1861d0b0621fea58de0d9e3d0317c07a56c3
rohitAmdahl <rohit_designer@outlook.com>
2024-03-27 22:51:45 +0100

    changed color - links

--- components/home/TabsLinks.tsx
+++ components/home/TabsLinks.tsx
@@ -5,15 +5,15 @@ const TabsLinks = () => {
   return (
     <div className="pt-8">
       <div className=" p-2  grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4  ">
-        <div className="bg-green-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-green-200">
+        <div className="bg-green-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-green-200">
           <Link
             href={"/dayrutine"}
             className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
           >
-            Day Routine at BarneHage
+            Day Routine
           </Link>
         </div>
-        <div className=" bg-fuchsia-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-fuchsia-200 ">
+        <div className=" bg-fuchsia-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-fuchsia-200 ">
           <Link
             href={"/indoor"}
             className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
@@ -21,7 +21,7 @@ const TabsLinks = () => {
             Indoor at BarneHage
           </Link>
         </div>
-        <div className=" bg-pink-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-pink-300">
+        <div className=" bg-pink-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-pink-200 ">
           <Link
             href={"/parentsadvice"}
             className="p-2  text-lg font-bold hover:underline hover:text-purple-950 "
@@ -29,7 +29,7 @@ const TabsLinks = () => {
             Parents Advice
           </Link>
         </div>
-        <div className=" bg-orange-50 text-center p-5 rounded-lg hover:ring-2 hover:bg-orange-200 ">
+        <div className=" bg-orange-300 text-center p-5 rounded-lg hover:ring-2 hover:bg-orange-200 ">
           <Link
             href={"/timing"}
             className="p-2  text-lg font-bold hover:underline hover:text-purple-950  "
