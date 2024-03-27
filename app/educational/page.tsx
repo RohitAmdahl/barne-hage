f8c2630022fa25d49a883de8b045640211db1844
rohitAmdahl <rohit_designer@outlook.com>
2024-03-27 12:34:21 +0100

    added metadata

--- app/educational/page.tsx
+++ app/educational/page.tsx
@@ -1,5 +1,10 @@
 import Learning from "@/components/education/Learning";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Educational page! Barnehage",
+  description: "educational page barnehage",
+};
 const EducationalSection = () => {
   return (
     <div className=" pt-8 container mx-auto max-w-4xl">
